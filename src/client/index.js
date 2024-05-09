f7d91ea6e7a69ff4607cf170a644653668e0677a
Richard Rigaude <richard.rigaud45@gmail.com>
2024-05-08 20:56:41 -0400

    webpack configuration

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,12 @@
+import {dropDownCard} from "./js/script.js"
+
+
 import './styles/base.scss'
 import './styles/dropcard.scss'
 import './styles/slider.scss'
 import './styles/ujamaa-card.scss'
 
-
-
-
 alert("Hey Y'all Richard is back at it again !!!");
 
 
-// export {dropDownCard}
-// export {resetCountdown}
-
+export {dropDownCard}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,57 +1,50 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
 const StylesLoader = require('styles-loader')
-const stylesLoader = new StylesLoader()
-// const { EnvironmentPlugin } = require('webpack');
-const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin'); 
+// const stylesLoader = new StylesLoader()
+const TerserPlugin = require('terser-webpack-plugin');
+const { EnvironmentPlugin } = require('webpack');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/client/index.js',
-  devtool: 'source-map',
-  stats: 'verbose',
-  output: {
-    libraryTarget:'var',
-    library: 'Client',
-    // path: path.resolve(process.cwd(), 'dist'),
-},
-module: {
-    rules: [
-        {
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
+    mode: 'production',
+    entry: './src/client/index.js',
+     output: {
+         libraryTarget: 'var',
+         library: 'Client'
+     },
+     optimization: {
+        minimizer: [new TerserPlugin({}), new CssMinimizerWebpackPlugin({})],
         },
-        {
-            test: /\.scss$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-        },
-        {
-            test: /\.html/,
-            loader: "html-loader",
-          },
+    module: {
+        rules: [
+            {
+                test:  /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
+        // new WorkboxPlugin.GenerateSW(),
+        new EnvironmentPlugin({
+            NODE_ENV: 'production'
+          })
     ]
-},
-plugins: [
-    new HtmlWebPackPlugin(({
-        template: "./src/client/views/index.html",
-        filename: "./index.html",
-    })),
-    new MiniCssExtractPlugin({ filename: "[name].css" }),
-    new CleanWebpackPlugin({
-          // Simulate the removal of files
-          dry: true,
-          // Write Logs to Console
-          verbose: true,
-          // Automatically remove all unused webpack assets on rebuild
-          cleanStaleWebpackAssets: true,
-          protectWebpackAssets: false
-    })
-]
 }
