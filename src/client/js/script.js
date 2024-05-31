d7651d0547d8dbac2a5c650aa710195429cb6d48
Richard Rigaude <richard.rigaud45@gmail.com>
2024-05-31 12:04:56 -0400

    added updated cariusel images

--- src/client/js/script.js
+++ src/client/js/script.js
@@ -7,4 +7,15 @@ function togglemenu(){
     subMenu.classList.toggle("open-menu");
 }
 
+let counter = 1;
+setInterval(function(){
+    document.getElementById('radio'+ counter).checked = true;
+    counter++;
+    if (counter > 8){
+        counter =1;
+    }
+}, 100000);
+
+
+
 export{togglemenu}
